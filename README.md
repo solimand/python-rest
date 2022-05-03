a9cfc0f7731f2c95860a782725c6554b99fdae45
Michele <michele.solimando@unibo.it>
2022-05-03 16:09:46 +0200

    GET installed app on device

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Simply python REST API call using ```request``` module
 # Requirements
 - python 3
 - module ```request```
+
+# Deployment
+Chain call:
+- POST[user, password] = TOKEN
+- GET_LIST_DEVICES[TOKEN] = DEV_ID
+- GET_INSTALLED_APP[TOKEN, DEV_ID] = find if app 'helloworld' is installed
--- caller.py
+++ caller.py
@@ -6,6 +6,8 @@ import json
 # api-endpoint
 ENDPOINT = "10.0.7.253:9443"
 LOGIN_URL = "https://"+ENDPOINT+"/portal/api/v1/login/direct"
+LS_DEV_URL = "https://"+ENDPOINT+"/portal/api/v1/devices"
+LA_APP_URL = "https://"+ENDPOINT+"/portal/api/v1/devices/installed-apps?"
 
 def main():
     # check args
@@ -31,10 +33,23 @@ def main():
 
     # extracting token
     api_access_token = r_login.json()["data"]["access_token"]
-    print("The received token is: %s" %str(api_access_token))
+    print("The received token is: %s\n" %str(api_access_token))
     getDevices_headers['authorization'] = api_access_token
 
-    # GET devices 
+    # GET devices (ASSUMPTION --> I consider the first ([0]) device of the list)
+    r_devices = requests.get(url = LS_DEV_URL, headers=getDevices_headers, verify=False)
+    deviceId = r_devices.json()["data"][0]["deviceId"]
+    print(deviceId)
+    
+    # GET installed apps on device
+    r_apps = requests.get(url = LA_APP_URL+"deviceid="+deviceId, headers=getDevices_headers, verify=False)
+    for a in r_apps.json()["data"]:
+        # print(a["title"]) #DEBUG
+        if (a["title"]=="helloworld"): #app already installed
+            print("App already installed. EXIT.")
+            return
+    
+    # install app
 
 if __name__ == "__main__":
     main()
