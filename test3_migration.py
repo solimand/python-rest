fb2f1085a1a51fefb14e5e459c80ca4bf1f91a1c
misolimando <m.solimand@gmail.com>
2022-09-21 15:16:11 +0200

    Added application control API

--- test3_migration.py
+++ test3_migration.py
@@ -4,6 +4,7 @@ import sys
 import requests
 import json
 
+#----------------------------------------------------------------------------
 # Endpoints
 MAN_ENDPOINT = "10.0.7.253:9443"
 MW_ENDPOINT = "10.0.7.132:8083"
@@ -12,13 +13,16 @@ VIR_DEV_ENDPOINT_1 = "10.0.7.249"
 VIR_DEV_ENDPOINT_2 = "10.0.7.60"
 
 # APIs
-IED_LOGIN_URL = "https://"+PHY_DEV_ENDPOINT+"/device/edge/api/v1/login/direct"
+IED_LOGIN_URL_PHY = "https://"+PHY_DEV_ENDPOINT+"/device/edge/api/v1/login/direct"
 IED_LOGIN_URL_VIR_1 = "https://"+VIR_DEV_ENDPOINT_1+"/device/edge/api/v1/login/direct"
 
-IED_SYS_INFO_URL = "https://"+PHY_DEV_ENDPOINT+"/device/edge/b.service/api/v1/system-info"
+IED_SYS_INFO_URL_PHY = "https://"+PHY_DEV_ENDPOINT+"/device/edge/b.service/api/v1/system-info"
+IED_SYS_INFO_URL_VIR_1 = "https://"+VIR_DEV_ENDPOINT_1+"/device/edge/b.service/api/v1/system-info"
 
 LIST_APP_DEV_VIR_1 = "https://" + VIR_DEV_ENDPOINT_1 + "/device/edge/b.service/api/v1/applications/search/pages/1?pageSize=100"
 LIST_APP_DEV_PHY = "https://" + PHY_DEV_ENDPOINT + "/device/edge/b.service/api/v1/applications/search/pages/1?pageSize=100"
+
+APP_CTRL_DEV_VIR_1 = "https://" + VIR_DEV_ENDPOINT_1 + "/device/edge/b.service/api/v1/applications/"
 
 # Threshold
 MAX_CPU_PERC = 30
@@ -27,23 +31,20 @@ MAX_MEM_PERC = 30
 # Other Globals
 APP_TO_CHECK = "stress"
 
+#----------------------------------------------------------------------------
+
 def main():
     # check args
     if (len(argv)<3):
-        print("usage -- python <prog_name> <ied_user> <ied_password>") #todo add docs
+        print("usage -- python <prog_name> <ied_user> <ied_password>") #TODO start with source->dest devices
         return    
     IED_USERNAME = argv[1]
     IED_USERPWD = argv[2]
 
-    # HEADERS and PAYLOADS (as Python dictionary)
-    dev_info_header = {'Authorization' : ''}
-
     # POST login
-    # ied_api_access_token = loginTo(IED_LOGIN_URL, IED_USERNAME,IED_USERPWD)
     ied_api_access_token = loginTo(IED_LOGIN_URL_VIR_1, IED_USERNAME,IED_USERPWD)
 
     # GET Dev Info
-    # dev_info_header['Authorization'] = ied_api_access_token
     # ied_res_dev_info = requests.get(url = IED_SYS_INFO_URL, headers=dev_info_header, verify=False)
     # if (ied_res_dev_info.status_code!=200):
     #     print("Something went wrong in the device info api. Error code = "+str(ied_res_dev_info.status_code))
@@ -51,34 +52,40 @@ def main():
     # ied_cpu_perc = ied_res_dev_info.json()["data"]["CpuPercentage"]
     # ied_mem_perc = ied_res_dev_info.json()["data"]["MemoryPercentage"]
 
-    # print("\nThe perc of MEM used on device %s is: %s\n" %(str(PHY_DEV_ENDPOINT), str(ied_mem_perc)))
-    # print("\nThe perc of CPU used on device %s is: %s\n" %(str(PHY_DEV_ENDPOINT), str(ied_cpu_perc)))
+    mem_usage, cpu_usage = devInfo(IED_SYS_INFO_URL_VIR_1, ied_api_access_token)
+    print("\nThe perc of MEM used on device %s is: %s\n" %(str(PHY_DEV_ENDPOINT), str(mem_usage)))
+    print("\nThe perc of CPU used on device %s is: %s\n" %(str(PHY_DEV_ENDPOINT), str(cpu_usage)))
 
+    # # CHECK Threshold
     # if (ied_mem_perc<MAX_CPU_PERC):
     #     print("No need for load balancing")
     #     return
 
     print("Trying to load balancing the device...")
 
+    # GET app ID - STOP app - UNINSTALL app
     appID = checkAppInstalled(LIST_APP_DEV_VIR_1, APP_TO_CHECK, ied_api_access_token)
-    print ("The App ID of the %s app is %s.\n" %(APP_TO_CHECK, appID))
-    #TODO APP LIST - GET APP ID - STOP&UNINSTALL
+    print ("The App ID of the %s app is %s\n" %(APP_TO_CHECK, appID))
+    if (appControl(APP_CTRL_DEV_VIR_1, ied_api_access_token, appID, "stop")):
+        print("App %s successfully stopped" %(APP_TO_CHECK))
+    if (appControl(APP_CTRL_DEV_VIR_1, ied_api_access_token, appID, "uninstall")):
+        print("App %s successfully uninstalled" %(APP_TO_CHECK))
+
 
     #TODO LOGIN TO SECOND DEVICE - DEPLOY APP
     
     return
 
+#----------------------------------------------------------------------------
 
 def loginTo (url, username, pwd):
     """
-    This func returns the login token
+    This func returns the login token (or ValueError in case of errors)
     """
     ied_login_data = {'username':username, 'password':pwd}
     login_header = {'content-type': 'application/json'}
     ied_res_login = requests.post(url = url, data=json.dumps(ied_login_data), headers=login_header, verify=False)
     if (ied_res_login.status_code!=200):
-        # print("Something went wrong in the login api. Error code = "+str(ied_res_login.status_code))
-        # return
         raise ValueError("Something went wrong in the login api. Error code = "+str(ied_res_login.status_code))
     api_access_token = ied_res_login.json()["data"]["access_token"]
     print("\nThe login token is: %s\n" %str(api_access_token))
@@ -87,7 +94,7 @@ def loginTo (url, username, pwd):
 
 def checkAppInstalled (url, appName, access_token):
     """
-    This func returns the app ID of the chosen app
+    This func returns the app ID of the chosen app (if it is installed on dev)
     """
     app_list_header = {'accept-language': 'en-US', 'authorization' : access_token}
     r_list_apps = requests.get(url = url, headers=app_list_header, verify=False)
@@ -98,6 +105,28 @@ def checkAppInstalled (url, appName, access_token):
             return a["applicationId"]
     return "app is not here!"
 
+
+def appControl (url, api_token, app_id, operation):
+    """
+    This function execute the requested operation on the app with ID app_id
+    Operation = stat | stop | uninstall | restart
+    """
+    app_ctrl_header = {'Authorization' : ''}
+    ied_res_app_stop = requests.post(url = APP_CTRL_DEV_VIR_1+app_id+"/"+operation, headers={'Authorization' : api_token}, verify=False)
+    if (ied_res_app_stop.status_code!=200):
+        raise ValueError("Something went wrong in the list app API. Error code = "+str(ied_res_app_stop.status_code))
+    return True
+
+
+def devInfo (url, api_token):
+    ied_res_dev_info = requests.get(url = url, headers={'Authorization' : api_token}, verify=False)
+    if (ied_res_dev_info.status_code!=200):
+        raise ValueError("Something went wrong in the dev info api. Error code = "+str(ied_res_dev_info.status_code))
+    ied_cpu_perc = ied_res_dev_info.json()["data"]["CpuPercentage"]
+    ied_mem_perc = ied_res_dev_info.json()["data"]["MemoryPercentage"]
+    return ied_cpu_perc, ied_mem_perc
+
+#----------------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
